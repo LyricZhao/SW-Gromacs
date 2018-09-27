085601b1c4b706ca205165ff27f793b6dd7b1051
Lyric Zhao <lyricz@yeah.net>
2018-09-27 13:54:51 +0800

    base version

--- submit.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-bsub -I -b -q q_sw_cpc -cgsp 64 -n 512 -np 4 -share_size 6500 -host_stack 500 -J GROMACS_LYRIC ~/zhaocg/gromacs-5.1.5/build/bin/mdrun_mpi_d -s ~/zhaocg/origin/ion_channel-st.tpr -v
